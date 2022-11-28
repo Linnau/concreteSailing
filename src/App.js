8b01b8920d7b88688b20d3d086f26b5a9dc84f94
Linnau <blah@linnau.dk>
2022-11-28 21:49:22 +0100

    Remove unneeded stuff from initial files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h2>Concrete Sailing</h2>
-      <h3>Some concrete sailing goin' strong</h3>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Concrete Sailing</h2>
+        <h3>Some concrete sailing goin' strong</h3>
+      </div>
+    );
+  }
 }
+
 export default App;
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
